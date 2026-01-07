fbc50aa4342fe4c1c0c42f76f8611e01d2fb00ae
Ayoub Douane <douaneayoubw@gmail.com>
2026-01-07 10:45:50 +0100

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/web2CMC/" : "/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const base =
+    mode === "production" ? process.env.VITE_BASE_PATH || "/web2CMC/" : "/";
+
+  return {
+    base,
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
